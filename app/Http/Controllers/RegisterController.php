5f9e8db1618436be598989f75c74bd2d447b62d2
Manshall <54L1114N@gmail.com>
2023-10-06 08:46:40 +0700

    register view

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class RegisterController extends Controller
 {
     public function index(){
-        return 'halo teman';
+        return view ('register');
     }
 
 }
